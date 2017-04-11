051ee6361ee26854e62504fde653eac0a30f791f
Jordan Tonni <jordantonni@outlook.com>
2017-04-11 19:49:16 +0100

    Chapter 6 finished

--- Items/Item 42 Ensure less of T means operator less.cpp
+++ Items/Item 42 Ensure less of T means operator less.cpp
@@ -1,10 +1,20 @@
 /*
- *
+ * Make sure less<T> ALWAYS calls operator< for type T
+ * 
+ * operator< is the default way to implement less<T>
+ * less<T> is the default comparison functor used for all sorting and searching 
+ * 
+ * Therefore operator< is the default.
+ * 
+ * If you want to sort objects using another criteria, don't specialize less<T> for your type T
+ * instead, create a new functor and pass it to the algorithim
+ * 
+ * e.g: set<Widget, MyComparisonFunctor> mySet;
  *
  *
  *
  * Summary:
- *  -
+ *  - Always ensure less<T> means operator< for your type   - It's what is always expected
  */
 
 
